378afe452ff68f810914261b895b5c503a9ade7c
Merielee <1025795@student.sae.edu.au>
2021-10-10 19:03:00 +1000

    Update Dice.cs

--- Assets/Dice.cs
+++ Assets/Dice.cs
@@ -1,31 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
 
-public class Dice : MonoBehaviour
-{// Defining new "variables"
-   public int    sides; // int means 'integer' or just whole numbers
-   public Color  colour;
-   public float  size; // float means 'floating point number' or just a number with decimals
-   public string nameOfDice; // string is for text
-  
-   // Roll the dice
-   // This is a function/method
-   // First word is the variable that you get BACK. "void" means it doesn't give you anything!
-   // Normal brackets () are variable it needs. These are called 'parameters'
-   // Note: This won't do ANYTHING until someone 'calls' it
-   void Roll()
-   {
-       // Unity's way of printing out a simple message to it's 'console' window
-       Debug.Log("Rolling!");
-       Debug.Log(sides);
-       Debug.Log(nameOfDice);
-   }
-
-   // Start is called when you click the play button in Unity
+public class Dice 
+{// Defining Dice functions
+   
+   int sides = 10;
+   
    void Start()
-   {
-       // Call the function
-       Roll();
-    
+
+   // Rolling the dice
+   
+   {Debug.Log(Random.Range (1, sides));
+   }
 }
