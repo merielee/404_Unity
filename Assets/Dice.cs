3c01dbd2e2f70c8c9e6e9572427aeb6678905b92
CameronBondeSAE <30306370+CameronBondeSAE@users.noreply.github.com>
2021-10-25 15:46:30 +1100

    Update Dice.cs

--- Assets/Dice.cs
+++ Assets/Dice.cs
@@ -5,32 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
+// Defining Dice functions
 public class Dice
-
-
-{// Defining Dice functions
-
-    int player1RandomNum;
-    int player2RandomNum;
-    int player1Points = 50;
-    int player2Points = 50;
-
-    public void Roll()
-
+{
+    public int Roll()
     {
         System.Random random = new System.Random();
 
-        for (int i = 0; i < 10; i++)
-        {
-            Debug.Log("Press any key to roll the dice");
-
-            player1RandomNum = random.Next(1, 7);
-            Debug.Log("Player 1 rolled a " + player1RandomNum);
-
-            player2RandomNum = random.Next(1, 7);
-            Debug.Log("Player 2 rolled a " + player2RandomNum);
-        }
-
+        return random.Next(1, 7);
     }
 
 }
